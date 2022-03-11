d0c8011b211488c8fc5f7bcb8ea75c8293f6749a
daraphista <daraphistadaone@gmail.com>
2022-03-11 19:20:02 +0800

    changed document title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,104 @@
 import { Component } from "react";
-import { AiOutlineUpload } from "react-icons/ai"
 import Button from "./components/Button";
+import EducationSection from "./components/EducationSection";
+import ExperienceSection from "./components/ExperienceSection";
+import PersonalInformationSection from "./components/PersonalInformationSection";
+import uniqid from "uniqid";
 
 class App extends Component {
   constructor (props) {
     super (props);
 
+    this.state = {
+      experiences: [
+        {
+          position: '',
+          company: '',
+          city: '',
+          from: '',
+          to: '',
+          id: uniqid(),
+        },
+      ],
+      educations: [
+        {
+          universityName: '',
+          city: '',
+          degree: '',
+          subject: '',
+          from: '',
+          to: '',
+          id: uniqid()
+        },
+      ]
+    }
+
+    this.handleDeleteExperience = this.handleDeleteExperience.bind(this);
+    this.handleDeleteEducation = this.handleDeleteEducation.bind(this);
+
+    this.handleAddExperience = this.handleAddExperience.bind(this);
+    this.handleAddEducation = this.handleAddEducation.bind(this);
+
+    this.handleChangeExperience = this.handleChangeExperience.bind(this);
+  }
+
+  handleDeleteExperience(selectedExperience) {
+    this.setState({
+      experiences: this.state.experiences.filter(experience => {
+        return experience.id !== selectedExperience.id;
+      })
+    })
+  }
+  
+  handleDeleteEducation(selectedEducation) {
+    this.setState({
+      educations: this.state.educations.filter(education => {
+        return education.id !== selectedEducation.id;
+      })
+    })
+  }
+
+  handleAddExperience() {
+    const newExperience = {
+      position: '',
+      company: '',
+      city: '',
+      from: '',
+      to: '',
+      id: uniqid(),
+    }
+
+    this.setState({
+      experiences: this.state.experiences.concat(newExperience)
+    })
+  }
+
+  handleAddEducation() {
+    const newEducation = {
+      universityName: '',
+      city: '',
+      degree: '',
+      subject: '',
+      from: '',
+      to: '',
+    }
+
+    this.setState({
+      educations: this.state.educations.concat(newEducation)
+    })
+  }
+
+  handleChangeExperience(input, property, index, arr) {
+    // making shallow copies of the array and item
+    let shallowArr = [...this.state[arr]];
+    let shallowItem = {...shallowArr[index]};
+    
+    // replacing property and storing in the array copy
+    shallowItem[property] = input;
+    shallowArr[index] = shallowItem;
+    
+    // setting the copy to the state
+    this.setState({[arr]: shallowArr})
   }
 
   render () {
@@ -17,103 +110,42 @@ class App extends Component {
 
             <section>
               <h2>Personal Information</h2>
-              <label htmlFor="first-name" className="focus-within:bg-red">
-                <input className="peer" type="text" id="first-name"></input>
-                <span className="peer-focus:invisible">First name</span>
-              </label>
-              <label htmlFor="last-name">
-                <input className="peer" type="text" id="last-name"></input>
-                <span className="peer-focus:invisible">Last name</span>
-              </label>
-              <label htmlFor="title">
-                <input className="peer" type="text" id="title"></input>
-                <span className="peer-focus:invisible">Title</span>
-              </label>
-              <label htmlFor="address">
-                <input className="peer" type="text" id="address"></input>
-                <span className="peer-focus:invisible">Address</span>
-              </label>
-              <label htmlFor="phone-number">
-                <input className="peer" type="number" id="phone-number"></input>
-                <span className="peer-focus:invisible">Phone number</span>
-              </label>
-              <label htmlFor="email">
-                <input className="peer" type="email" id="email"></input>
-                <span className="peer-focus:invisible">Email</span>
-              </label>
-              <label htmlFor="description">
-                <textarea className="resize-none peer h-[100%]" id="description"></textarea>
-                <span className="peer-focus:invisible top-3.5">Description</span>
-              </label>
-              <label htmlFor="photo" className="bg-gray-100 rounded cursor-pointer hover:shadow-md transition-shadow active:shadow-none">
-                <span className="flex items-center gap-1">Photo<AiOutlineUpload /></span>
-                <input className="invisible" type="file" id="photo"></input>
-              </label>
+              <PersonalInformationSection />
             </section>
 
             <section className="experience">
               <h2>Experience</h2>
-              <section>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">Position</span>
-                </label>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">Company</span>
-                </label>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">City</span>
-                </label>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">From</span>
-                </label>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">To</span>
-                </label>
-              </section>
-              <Button type="Add" /> 
-              <Button type="Delete" /> 
+              {this.state.experiences.map((experience) => {
+                return <ExperienceSection 
+                  array="experiences" 
+                  index={this.state.experiences.indexOf(experience)} 
+                  handleDelete={this.handleDeleteExperience} 
+                  handleChange={this.handleChangeExperience} 
+                  experience={experience}
+                  id={experience.id}
+                />
+              })}
+              <Button type="Add" handleClick={this.handleAddExperience}/> 
             </section>
 
-            <section class="education">
-              <section>
-                <h2>Education</h2>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">University name</span>
-                </label>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">City</span>
-                </label>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">Degree</span>
-                </label>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">Subject</span>
-                </label>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">From</span>
-                </label>
-                <label>
-                  <input className="peer" type="text"></input>
-                  <span className="peer-focus:invisible">To</span>
-                </label>
-              </section>
-              <Button type="Add" />
-              <Button type="Delete" />
+            <section className="education">
+              <h2>Education</h2>
+              {this.state.educations.map((education) => {
+                return <EducationSection 
+                  array="educations"
+                  index={this.state.educations.indexOf(education)}
+                  handleDelete={this.handleDeleteEducation}
+                  handleChange={this.handleChangeExperience}
+                  education={education}
+                />
+              })}
+              <Button type="Add" handleClick={this.handleAddEducation}/>
             </section>
           
             <section>
 
             </section>
+
           </form>
         </div>
 
