f337411dea9bcb907283d2adf44ce6e467c261e1
daraphista <daraphistadaone@gmail.com>
2022-03-20 10:12:31 +0800

    converted App into functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { useState } from "react";
 import Button from "./components/Button";
 import EducationSection from "./components/EducationSection";
 import ExperienceSection from "./components/ExperienceSection";
@@ -6,72 +6,63 @@ import PersonalInformationSection from "./components/PersonalInformationSection"
 import PreviewSection from "./components/PreviewSection";
 import uniqid from "uniqid";
 
-class App extends Component {
-  constructor (props) {
-    super (props);
+const App = () => {
+  const [personalInformation, setPersonalInformation] = useState(
+    [
+      {
+        firstName: '',
+        lastName: '',
+        title: '',
+        address: '',
+        phoneNumber: '',
+        email: '',
+        description: '',
+        id: uniqid(),
+      },
+    ]
+  )
+  const [experiences, setExperiences] = useState(
+    [
+      {
+        position: '',
+        company: '',
+        city: '',
+        from: '',
+        to: '',
+        id: uniqid(),
+      },
+    ]
+  )
+  const [educations, setEducations] = useState(
+    [
+      {
+        universityName: '',
+        city: '',
+        degree: '',
+        subject: '',
+        from: '',
+        to: '',
+        id: uniqid()
+      },
+    ]
+  )
 
-    this.state = {
-      personalInformation: [ // could've used an object, still used array for compatibility with the handleChange function
-        {
-          firstName: '',
-          lastName: '',
-          title: '',
-          address: '',
-          phoneNumber: '',
-          email: '',
-          description: '',
-          id: uniqid(),
-        },
-      ],
-      experiences: [
-        {
-          position: '',
-          company: '',
-          city: '',
-          from: '',
-          to: '',
-          id: uniqid(),
-        },
-      ],
-      educations: [
-        {
-          universityName: '',
-          city: '',
-          degree: '',
-          subject: '',
-          from: '',
-          to: '',
-          id: uniqid()
-        },
-      ]
-    }
-
-    this.handleDeleteExperience = this.handleDeleteExperience.bind(this);
-    this.handleDeleteEducation = this.handleDeleteEducation.bind(this);
-
-    this.handleAddExperience = this.handleAddExperience.bind(this);
-    this.handleAddEducation = this.handleAddEducation.bind(this);
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleDeleteExperience(selectedExperience) {
-    this.setState({
-      experiences: this.state.experiences.filter(experience => {
+  const handleDeleteExperience = (selectedExperience) => {
+    setExperiences(
+      experiences.filter(experience => {
         return experience.id !== selectedExperience.id;
       })
-    })
+    )
   }
-  
-  handleDeleteEducation(selectedEducation) {
-    this.setState({
-      educations: this.state.educations.filter(education => {
+  const handleDeleteEducation = (selectedEducation) => {
+    setEducations(
+      educations.filter(education => {
         return education.id !== selectedEducation.id;
       })
-    })
+    )
   }
 
-  handleAddExperience() {
+  const handleAddExperience = () => {
     const newExperience = {
       position: '',
       company: '',
@@ -81,12 +72,9 @@ class App extends Component {
       id: uniqid(),
     }
 
-    this.setState({
-      experiences: this.state.experiences.concat(newExperience)
-    })
+    setExperiences(experiences.concat(newExperience));
   }
-
-  handleAddEducation() {
+  const handleAddEducation = () => {
     const newEducation = {
       universityName: '',
       city: '',
@@ -96,14 +84,12 @@ class App extends Component {
       to: '',
     }
 
-    this.setState({
-      educations: this.state.educations.concat(newEducation)
-    })
+    setEducations(educations.concat(newEducation));
   }
 
-  handleChange(input, property, index, arr) {
+  const handleChangePersonalInformation = (input, property, index, arr) => {
     // making shallow copies of the array and item
-    let shallowArr = [...this.state[arr]];
+    let shallowArr = [...personalInformation];
     let shallowItem = {...shallowArr[index]};
     
     // replacing property and storing in the array copy
@@ -111,104 +97,123 @@ class App extends Component {
     shallowArr[index] = shallowItem;
     
     // setting the copy to the state
-    this.setState({[arr]: shallowArr})
+    setPersonalInformation(shallowArr);
+  }
+  const handleChangeExperience = (input, property, index, arr) => {
+    // making shallow copies of the array and item
+    let shallowArr = [...experiences];
+    let shallowItem = {...shallowArr[index]};
+    
+    // replacing property and storing in the array copy
+    shallowItem[property] = input;
+    shallowArr[index] = shallowItem;
+    
+    // setting the copy to the state
+    setExperiences(shallowArr)
+  }
+  const handleChangeEducation = (input, property, index, arr) => {
+    // making shallow copies of the array and item
+    let shallowArr = [...educations];
+    let shallowItem = {...shallowArr[index]};
+    
+    // replacing property and storing in the array copy
+    shallowItem[property] = input;
+    shallowArr[index] = shallowItem;
+    
+    // setting the copy to the state
+    setEducations(shallowArr)
   }
 
-  render () {
+  return (
+    <div className="bg-gray-200 p-8 flex">
 
-    const { personalInformation } = this.state;
+      <div className="w-[50%] flex justify-center">
+        <form className="bg-white flex flex-col items-center w-[90%] rounded shadow-lg gap-6">
 
-    return (
-      <div className="bg-gray-200 p-8 flex">
+          <section>
+            <h2>Personal Information</h2>
+            {personalInformation.map((personalInfo) => {
+              return <PersonalInformationSection 
+                array="personalInformation"
+                index={personalInformation.indexOf(personalInfo)}
+                handleChange={handleChangePersonalInformation}
+                personalInfo={personalInfo}
+                id={personalInfo.id}
+              />
+            })}
+          </section>
 
-        <div className="w-[50%] flex justify-center">
-          <form className="bg-white flex flex-col items-center w-[90%] rounded shadow-lg gap-6">
+          <section className="experience">
+            <h2>Experience</h2>
+            {experiences.map((experience) => {
+              return <ExperienceSection 
+                array="experiences" 
+                index={experiences.indexOf(experience)} 
+                handleDelete={handleDeleteExperience} 
+                handleChange={handleChangeExperience} 
+                experience={experience}
+                id={experience.id}
+              />
+            })}
+            <Button type="Add" handleClick={handleAddExperience}/> 
+          </section>
+
+          <section className="education">
+            <h2>Education</h2>
+            {educations.map((education) => {
+              return <EducationSection 
+                array="educations"
+                index={educations.indexOf(education)}
+                handleDelete={handleDeleteEducation}
+                handleChange={handleChangeEducation}
+                education={education}
+              />
+            })}
+            <Button type="Add" handleClick={handleAddEducation}/>
+          </section>
+
+        </form>
+      </div>
+
+      <div className="w-[50%] flex justify-center">
+        <div className="bg-white w-[90%] grid grid-cols-[repeat(10,_minmax(0,_1fr))] grid-rows-6 shadow-lg">
+          <div className="bg-blue-800 row-[1/2] col-[1/11] flex flex-col justify-center p-6 gap-2">
+            <h1 className="text-white bold text-5xl">{`${personalInformation[0].firstName} ${personalInformation[0].lastName}`}</h1>
+            <h2 className="text-white">{`${personalInformation[0].title}`}</h2>
+          </div>
+
+          <div className="bg-gray-100 row-[2/7] col-[8/11] p-6">
+            <h2 className="text-blue-800">Personal Details</h2>
+            <p>{`${personalInformation[0].address}`}</p>
+            <p>{`${personalInformation[0].phoneNumber}`}</p>
+            <p>{`${personalInformation[0].email}`}</p>
+          </div>
+
+          <div className="flex flex-col row-[2/7] col-[1/8] gap-3 p-6">
+            <section>
+              <h2 className="text-blue-800">Description</h2>
+              <p className="italic">{`${personalInformation[0].description}`}</p>
+            </section>
 
             <section>
-              <h2>Personal Information</h2>
-              {this.state.personalInformation.map((personalInfo) => {
-                return <PersonalInformationSection 
-                  array="personalInformation"
-                  index={this.state.personalInformation.indexOf(personalInfo)}
-                  handleChange={this.handleChange}
-                  personalInfo={personalInfo}
-                  id={personalInfo.id}
-                />
+              <h2 className="text-blue-800">Experience</h2>
+              {experiences.map((experience) => {
+                return <PreviewSection isExperience={true} experience={experience} />
               })}
             </section>
 
-            <section className="experience">
-              <h2>Experience</h2>
-              {this.state.experiences.map((experience) => {
-                return <ExperienceSection 
-                  array="experiences" 
-                  index={this.state.experiences.indexOf(experience)} 
-                  handleDelete={this.handleDeleteExperience} 
-                  handleChange={this.handleChange} 
-                  experience={experience}
-                  id={experience.id}
-                />
+            <section>
+              <h2 className="text-blue-800">Education</h2>
+              {educations.map((education) => {
+                return <PreviewSection isEducation={true} education={education} />
               })}
-              <Button type="Add" handleClick={this.handleAddExperience}/> 
             </section>
-
-            <section className="education">
-              <h2>Education</h2>
-              {this.state.educations.map((education) => {
-                return <EducationSection 
-                  array="educations"
-                  index={this.state.educations.indexOf(education)}
-                  handleDelete={this.handleDeleteEducation}
-                  handleChange={this.handleChange}
-                  education={education}
-                />
-              })}
-              <Button type="Add" handleClick={this.handleAddEducation}/>
-            </section>
-
-          </form>
-        </div>
-
-        <div className="w-[50%] flex justify-center">
-          <div className="bg-white w-[90%] grid grid-cols-[repeat(10,_minmax(0,_1fr))] grid-rows-6 shadow-lg">
-            <div className="bg-blue-800 row-[1/2] col-[1/11] flex flex-col justify-center p-6 gap-2">
-              <h1 className="text-white bold text-5xl">{`${personalInformation[0].firstName} ${personalInformation[0].lastName}`}</h1>
-              <h2 className="text-white">{`${personalInformation[0].title}`}</h2>
-            </div>
-
-            <div className="bg-gray-100 row-[2/7] col-[8/11] p-6">
-              <h2 className="text-blue-800">Personal Details</h2>
-              <p>{`${personalInformation[0].address}`}</p>
-              <p>{`${personalInformation[0].phoneNumber}`}</p>
-              <p>{`${personalInformation[0].email}`}</p>
-            </div>
-
-            <div className="flex flex-col row-[2/7] col-[1/8] gap-3 p-6">
-              <section>
-                <h2 className="text-blue-800">Description</h2>
-                <p className="italic">{`${personalInformation[0].description}`}</p>
-              </section>
-
-              <section>
-                <h2 className="text-blue-800">Experience</h2>
-                {this.state.experiences.map((experience) => {
-                  return <PreviewSection isExperience={true} experience={experience} />
-                })}
-              </section>
-
-              <section>
-                <h2 className="text-blue-800">Education</h2>
-                {this.state.educations.map((education) => {
-                  return <PreviewSection isEducation={true} education={education} />
-                })}
-              </section>
-            </div>
           </div>
         </div>
-
       </div>
-    )
-  }
+
+    </div>
+  )
 }
 
 export default App;
