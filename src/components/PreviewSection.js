39f909bb805efcd90029e353b917326b8f97c7a4
daraphista <daraphistadaone@gmail.com>
2022-03-20 10:44:57 +0800

    converted PreviewSection into functional component

--- src/components/PreviewSection.js
+++ src/components/PreviewSection.js
@@ -1,32 +1,23 @@
-import { Component } from "react";
+import React from "react";
 
-class PreviewSection extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-
-    console.log(this.props.experience);
-
-    if(this.props.isExperience) {
-      return (
-        <section className="grid grid-cols-[minmax(0,_1fr)_minmax(0,_2fr)]">
-          <h2 className="col-[1/2]">{`${this.props.experience.from} - ${this.props.experience.to}`}</h2>
-          <h2 className="col-[2/3]">{this.props.experience.position}</h2>
-          <p className="col-[2/3]">{`${this.props.experience.company}, ${this.props.experience.city}`}</p>
-        </section>
-      )
-    } else if(this.props.isEducation) {
-      return (
-        <section className="grid grid-cols-[minmax(0,_1fr)_minmax(0,_2fr)]">
-          <h2 className="col-[1/2]">{`${this.props.education.from} - ${this.props.education.to}`}</h2>
-          <h2 className="col-[2/3]">{`${this.props.education.universityName}, ${this.props.education.city}`}</h2>
-          <p className="col-[2/3]">{`Degree: ${this.props.education.degree}`}</p>
-          <p className="col-[2/3]">{`Subject: ${this.props.education.subject}`}</p>
-        </section>
-      )
-    }
+const PreviewSection = (props) => {
+  if(props.isExperience) {
+    return (
+      <section className="grid grid-cols-[minmax(0,_1fr)_minmax(0,_2fr)]">
+        <h2 className="col-[1/2]">{`${props.experience.from} - ${props.experience.to}`}</h2>
+        <h2 className="col-[2/3]">{props.experience.position}</h2>
+        <p className="col-[2/3]">{`${props.experience.company}, ${props.experience.city}`}</p>
+      </section>
+    )
+  } else if(props.isEducation) {
+    return (
+      <section className="grid grid-cols-[minmax(0,_1fr)_minmax(0,_2fr)]">
+        <h2 className="col-[1/2]">{`${props.education.from} - ${props.education.to}`}</h2>
+        <h2 className="col-[2/3]">{`${props.education.universityName}, ${props.education.city}`}</h2>
+        <p className="col-[2/3]">{`Degree: ${props.education.degree}`}</p>
+        <p className="col-[2/3]">{`Subject: ${props.education.subject}`}</p>
+      </section>
+    )
   }
 }
 
